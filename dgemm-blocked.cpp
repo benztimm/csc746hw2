f768d54fda78afc59c1b01b2d99cd6997061ad7f
benztimm <ebuddharuksa@sfsu.edu>
2023-09-17 15:04:26 -0700

    do copy optimize

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -6,19 +6,39 @@ const char *dgemm_desc = "Blocked dgemm.";
  * On exit, A and B maintain their input values. */
 void square_dgemm_blocked(int n, int block_size, double *A, double *B, double *C)
 {
-   // These outer loops iterate over the blocks of the matrices.
-   // si, sj, and sk represent the starting row, column, and depth indices of the current blocks.
-   for (int si = 0; si < n; si += block_size)
-      for (int sj = 0; sj < n; sj += block_size)
-         for (int sk = 0; sk < n; sk += block_size)
-            for (int i = si; i < si + block_size; ++i)
-               for (int j = sj; j < sj + block_size; ++j)
-               {
-                  // Initialize the accumulator with the current value in matrix C.
-                  double cij = C[i + j * n]; // C is in column-major format
-                  // k loop computes the dot product for the blocks of the i-th row of A and the j-th column of B.
-                  for (int k = sk; k < sk + block_size; ++k)
-                     cij += A[i + k * n] * B[k + j * n]; // A and B are in column-major format
-                  C[i + j * n] = cij;                    // Store the accumulated result back in matrix C.
-               }
+    double A_block[block_size * block_size];
+    double B_block[block_size * block_size];
+
+    for (int si = 0; si < n; si += block_size)
+    {
+        for (int sj = 0; sj < n; sj += block_size)
+        {
+            for (int sk = 0; sk < n; sk += block_size)
+            {
+                // Copy blocks of A and B into local storage
+                for (int i = 0; i < block_size; ++i)
+                {
+                    for (int j = 0; j < block_size; ++j)
+                    {
+                        A_block[i + j * block_size] = A[(si + i) + (sk + j) * n];
+                        B_block[i + j * block_size] = B[(sk + i) + (sj + j) * n];
+                    }
+                }
+
+                // Multiply the individual blocks using local storage
+                for (int i = 0; i < block_size; ++i)
+                {
+                    for (int j = 0; j < block_size; ++j)
+                    {
+                        double cij = C[(si + i) + (sj + j) * n];
+                        for (int k = 0; k < block_size; ++k)
+                        {
+                            cij += A_block[i + k * block_size] * B_block[k + j * block_size];
+                        }
+                        C[(si + i) + (sj + j) * n] = cij;
+                    }
+                }
+            }
+        }
+    }
 }
